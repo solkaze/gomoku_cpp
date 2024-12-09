98a33ac75a8295a1e789c0fdd0868d1880c51ed4
solkaze <t.yoichi.0805@gmail.com>
2024-12-10 02:45:32 +0900

    ベースマスク生成

--- src/prohibited.cpp
+++ src/prohibited.cpp
@@ -1,49 +1,23 @@
+#include <array>
+
 #include "common.hpp"
 #include "prohibited.hpp"
 
+constexpr array<uint8_t, 3> BASE_3MASK = {
+    0b01110,
+    0b010110,
+    0b011010,
+};
+
+constexpr array<uint8_t, 4> BASE_4MASK = {
+    0b011110,
+    0b0101110,
+    0b0110110,
+    0b0111010,
+};
+
 // 33禁判定
 bool isProhibitedThreeThree(const BitBoard& bitBoard, int y, int x) {
-    int threeCount = 0;
-
-    for (const auto& [dy, dx] : DIRECTIONS) {
-        int count = 1;
-        int openCount = 0;
-
-        // 正方向
-        for (int step = 1; step < 5; ++step) {
-            int ny = y + step * dy;
-            int nx = x + step * dx;
-
-            if (bitBoard.isInBounds(y, x)) break; // 画面外
-
-            if (BitBoard::checkEmptyBit(y, x)) { // 空白
-
-            } else if (bitBoard.checkBit(y, x)) { // 自分
-                ++count;
-            } else break; // 相手
-        }
-
-        // 負方向
-        for (int step = 1; step < 5; ++step) {
-            int ny = y - step * dy;
-            int nx = x - step * dx;
-
-            if (bitBoard.isInBounds(y, x)) break; // 画面外
-
-            if (BitBoard::checkEmptyBit(y, x)) { // 空白
-
-            } else if (bitBoard.checkBit(y, x)) { // 自分
-                ++count;
-            } else break; // 相手
-
-            ++count;
-        }
-
-        if (count == 3) ++threeCount;
-    }
-
-    if (threeCount >= 2) return true;
-    else return false;
 }
 
 // 44禁判定
